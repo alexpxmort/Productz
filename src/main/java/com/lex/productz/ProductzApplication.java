62987776d88b18fcc22a8406e1e4f754f4297cba
alexpxmort <lexpdigi@gmail.com>
2021-04-20 15:30:42 -0300

    first commit

---
+++ src/main/java/com/lex/productz/ProductzApplication.java
@@ -0,0 +1,13 @@
+package com.lex.productz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lex/productz/ProductzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex.productz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
