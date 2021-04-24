19d866fbd4811b2d3c6ba77184c22baff8ad7386
alexpxmort <lexpdigi@gmail.com>
2021-04-24 18:50:39 -0300

    add test unitario delete

--- src/main/resources/sql/produtoControllerTest.sql
+++ src/main/resources/sql/produtoControllerTest.sql
@@ -1,2 +1,2 @@
-INSERT INTO produtos (id,nome,codigo_barra,descricao,categoria,quantidade)
-VALUES(1,'teste nome','151515','teste descricao','A TESTE',1000);
+INSERT INTO produtos (nome,codigo_barra,descricao,categoria,quantidade)
+VALUES('teste nome','151515','teste descricao','A TESTE',1000);
--- src/test/java/com/lex/productz/ProdutoControllerTest.java
+++ src/test/java/com/lex/productz/ProdutoControllerTest.java
@@ -1,8 +1,11 @@
 package com.lex.productz;
 import static  org.junit.Assert.assertEquals;
+import static  org.junit.Assert.assertFalse;
 
 import java.util.Optional;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +28,28 @@ public class ProdutoControllerTest {
 	private ProdutoRepository produtoRepositoryTest;
 	
 	@Test
+	@Before(value = "")
 	public void findTest()  throws ProdutoNotFoundException{
 		Optional<Produto>  p = produtoRepositoryTest.findById(1);
 		
 		assertEquals(1,p.get().getId());
 		assertEquals("151515",p.get().getCodigoBarra());
+	}
+	
+
+	@Test
+	@After
+	public void deleteTest()  throws ProdutoNotFoundException{
+		Optional<Produto>  p = produtoRepositoryTest.findById(1);
+		
+		if(p.isPresent()) {
+			produtoRepositoryTest.delete(p.get());
+			
+			Optional<Produto>  pDel = produtoRepositoryTest.findById(1);
+			
+			assertFalse(pDel.isPresent());
+			
+		}
 	}
 	
 }
